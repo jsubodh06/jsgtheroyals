ec2cd83bad72a28d9e8f51a7a20102e328dd22d2
jsubodh06 <38216525+jsubodh06@users.noreply.github.com>
2025-11-22 03:06:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/jsgtheroyals/',
+  base: "/",
   server: {
     host: "::",
     port: 8080,
